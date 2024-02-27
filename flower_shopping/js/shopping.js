804b703d38187793d2e5557760aa2a00c21821c3
Leosparroww <leosparrow.yota@gmail.com>
2024-02-27 14:52:54 +0630

    Update shopping.js

--- flower_shopping/js/shopping.js
+++ flower_shopping/js/shopping.js
@@ -92,7 +92,7 @@ async function categoryData(url) {
       console.log(error);
     });
 }
-categoryData("http://localhost:8000/api/category");
+categoryData("https://fdfg.theorymm.com/api/category");
 //product data
 async function productData(url) {
   let res = await fetch(url)
@@ -106,7 +106,7 @@ async function productData(url) {
       console.log(error);
     });
 }
-productData("http://localhost:8000/api/item");
+productData("https://fdfg.theorymm.com/api/item");
 
 // for view box
 const viewBox = document.querySelectorAll("[data-view-box] div");
@@ -174,7 +174,7 @@ function quickViewFunc(id) {
                                     `;
     }
   });
-  productQuickViewCard.innerHTML = `<div class="quick-img" bis_skin_checked="1"><img src="http://localhost:8000/storage/image/${
+  productQuickViewCard.innerHTML = `<div class="quick-img" bis_skin_checked="1"><img src="https://fdfg.theorymm.com/storage/image/${
     data.image.split(",")[0]
   }" alt=""></div>
                         <div class="quick-article" bis_skin_checked="1">
@@ -435,7 +435,7 @@ function productListApply(productLists) {
                         <div class="product-img"><a href="/productPage.html?id=${
                           list.product_id
                         }"><span hidden disabled>${list.product_id}</span><img
-                                    src="http://localhost:8000/storage/image/${
+                                    src="https://fdfg.theorymm.com/storage/image/${
                                       list.image.split(",")[0]
                                     }" alt=""></a>
                             <div class="product-icon">
@@ -571,7 +571,7 @@ const cartSideBar = (pp) => {
     cartHtml2 += `
                     <div class="sd-cart-product">
                         <img 
-                                    src="http://localhost:8000/storage/image/${
+                                    src="https://fdfg.theorymm.com/storage/image/${
                                       cartProduc.image.split(",")[0]
                                     }" alt="">
                         <div class="sd-cart-descript">
